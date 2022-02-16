208df120d78cadc173175183e94a29ce9fd40d1f
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2022-02-16 23:39:00 +0900

    eslint追加

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    extends: ['plugin:react/recommended', 'prettier'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 12,
+      sourceType: 'module',
+    },
+    plugins: ['react'],
+    settings: {},
+    rules: {
+      'react/prop-types': 0,
+      'react/jsx-props-no-spreading': [
+        2,
+        {
+          html: 'enforce',
+          custom: 'ignore',
+          explicitSpread: 'enforce',
+        },
+      ],
+    },
+};
+  
